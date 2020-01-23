45e03ee54c1cd12a2c0d13541816803940a84d5b
ilyes12zouaoui <ilyes.zouaoui@esprit.tn>
2020-01-23 19:09:29 +0100

    adding DFS and BFS to the Graph data structure

--- data structures/Graph.js
+++ data structures/Graph.js
@@ -38,6 +38,75 @@ class Graph {
     delete this.adjacentList[value];
     this.nodes--;
   }
+
+  DFSRecursively(start) {
+    let result = [];
+    let visited = {};
+    const helper = node => {
+      //put node in visited and result
+      result.push(node);
+      visited[node] = true;
+      //go throught childrens and check if child is not visited then visit it recursively
+      for (let child of this.adjacentList[node]) {
+        if (!visited[child]) {
+          helper(child);
+        }
+      }
+      return;
+    };
+
+    helper(start);
+
+    return result;
+  }
+
+  DFSiteratively(start) {
+    //we have result and visited
+    //and we use a stack where we put nodes to visit
+    let result = [];
+    let visited = { [start]: true };
+
+    let toVisit = [start];
+    //while there is a node to visit
+    while (toVisit.length != 0) {
+      let currentNode = toVisit.pop();
+      //put that node in visited and result
+      result.push(currentNode);
+      //loop throught it's children and if one of them is not visited put it inside the toVisit stuck
+      for (let child of this.adjacentList[currentNode]) {
+        if (!visited[child]) {
+          visited[child] = true;
+          toVisit.push(child);
+        }
+      }
+    }
+    return result;
+  }
+
+  BFS(start) {
+    //visited nodes
+    let visited = { [start]: true };
+    //result
+    let result = [];
+
+    //queue that contains nodes to visit
+    let toVisit = [start];
+
+    //while there is a node in queue
+    while (toVisit.length != 0) {
+      let current = toVisit.shift();
+      result.push(current);
+
+      for (let node of this.adjacentList[current]) {
+        if (!visited[node]) {
+          visited[node] = true;
+          toVisit.push(node);
+        }
+      }
+    }
+
+    return result;
+  }
 }
 
 /*
@@ -46,6 +115,8 @@ graph to create:
     0 - 3
    / \
   1 - 2
+  |
+  4
 
 */
 
@@ -54,16 +125,21 @@ graph.addVertex(0);
 graph.addVertex(1);
 graph.addVertex(2);
 graph.addVertex(3);
+graph.addVertex(4);
 
 graph.addEdge(0, 1);
 graph.addEdge(0, 2);
 graph.addEdge(0, 3);
 graph.addEdge(1, 2);
+graph.addEdge(1, 4);
 
 console.log(graph);
+console.log(graph.DFSRecursively(0));
+console.log(graph.DFSiteratively(0));
+console.log(graph.BFS(4));
 
-graph.removeEdge(0, 2);
-console.log(graph);
+// graph.removeEdge(0, 2);
+// console.log(graph);
 
-graph.removeVertex(1);
-console.log(graph);
+// graph.removeVertex(1);
+// console.log(graph);
